fd9d7e05969970d51919b0111790053350753022
vanduc19it <duc19it@gmail.com>
2023-04-06 10:08:45 +0700

    init

---
+++ connect.js
@@ -0,0 +1,25 @@
+var sql = require('mssql/msnodesqlv8');
+
+
+ const config = {
+  user: 'sa',
+  password: '123',
+  server: 'localhost'  || "127.0.0.1", 
+  database: 'QLTHUVIEN',
+//   port: 1433, 
+  options: {
+    trustedConnection: true,
+
+  },
+
+};
+
+const conn = new sql.ConnectionPool(config).connect().then((pool)=> {
+    return pool;
+});
+
+
+module.exports = {
+    conn : conn,
+    sql: sql,
+}
---
+++ server.js
@@ -0,0 +1,50 @@
+
+const {conn, sql} = require('./connect');
+const express = require('express')
+
+var cors = require('cors')
+const router = express.Router()
+const app = express()
+app.use(cors()) 
+const port = 5000;
+
+// const pool = await conn;
+
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
+
+
+app.get('/student',async (req, res) => {
+  const pool = await conn;
+    const sqlString = "SELECT * FROM SINHVIEN"
+    await pool.request().query(sqlString, function(err, data) {
+        console.log(err, data)
+        res.send(data.recordsets[0])
+      });
+    
+})
+
+
+app.post('/addCategory', async (req, res) => {
+  const pool = await conn;
+  const sqlString = "INSERT INTO DANHMUC (MaDanhMuc, TenDanhMuc) VALUES('estmadanhmuc',estmadanhmuc)"
+  console.log(req.body) ; 
+  await pool.request().query()
+  .input('MaDanhMuc', sql.Int, req.body.MaDanhMuc)
+  .input('TenDanhMuc', sql.NVarChar, req.body.TenDanhMuc)
+  .query(sqlString, function (err,data) {
+    res.send( data);
+  });
+    
+    
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })

66e860925198e35296b31b79070989ecf9c5c7db
vanduc19it <duc19it@gmail.com>
2023-04-23 19:41:17 +0700

    authorization for admin and employee

--- connect.js
+++ connect.js
@@ -1,25 +1,99 @@
-var sql = require('mssql/msnodesqlv8');
+var sql = require("mssql/msnodesqlv8");
+// var { roleData } = require("./singleton");
 
+const fs = require("fs");
+const data = fs.readFileSync("data.txt", "utf-8");
 
- const config = {
-  user: 'sa',
-  password: '123',
-  server: 'localhost'  || "127.0.0.1", 
-  database: 'QLTHUVIEN2',
-//   port: 1433, 
+const roleData = JSON.parse(data)[0];
+console.log("daaata", roleData);
+
+const config = {
+  user: "sa",
+  password: "123",
+  server: "localhost" || "127.0.0.1",
+  database: "QLTHUVIEN2",
+  //   port: 1433,
   options: {
-    "trustedConnection": true,
-    "trustServerCertificate": true
+    trustedConnection: true,
+    trustServerCertificate: true,
   },
-
 };
 
-const conn = new sql.ConnectionPool(config).connect().then((pool)=> {
-    return pool;
-});
+const configWithSever1 = {
+  user: "sa",
+  password: "123",
+  server: "NITRO5-DUCIT\\MSSQLSERVER01",
+  database: "QLTHUVIEN2",
+  //   port: 1433,
+  options: {
+    trustedConnection: true,
+    trustServerCertificate: true,
+  },
+};
+const configWithSever2 = {
+  user: "sa",
+  password: "123",
+  server: "NITRO5-DUCIT\\MSSQLSERVER02",
+  database: "QLTHUVIEN2",
+  //   port: 1433,
+  options: {
+    trustedConnection: true,
+    trustServerCertificate: true,
+  },
+};
+const configWithSever3 = {
+  user: "sa",
+  password: "123",
+  server: "NITRO5-DUCIT\\MSSQLSERVER03",
+  database: "QLTHUVIEN2",
+  //   port: 1433,
+  options: {
+    trustedConnection: true,
+    trustServerCertificate: true,
+  },
+};
+const configWithSever4 = {
+  user: "sa",
+  password: "123",
+  server: "NITRO5-DUCIT\\MSSQLSERVER04",
+  database: "QLTHUVIEN2",
+  //   port: 1433,
+  options: {
+    trustedConnection: true,
+    trustServerCertificate: true,
+  },
+};
 
+// let conn = null;
+// console.log({ rolll: roleData.data });
+
+if (roleData.role == 1) {
+  conn = new sql.ConnectionPool(config).connect().then((pool) => {
+    return pool;
+  });
+} else if (roleData.role == 2 && roleData.uniID == 1) {
+  conn = new sql.ConnectionPool(configWithSever1).connect().then((pool) => {
+    return pool;
+  });
+} else if (roleData.role == 2 && roleData.uniID == 2) {
+  conn = new sql.ConnectionPool(configWithSever2).connect().then((pool) => {
+    return pool;
+  });
+} else if (roleData.role == 2 && roleData.uniID == 3) {
+  conn = new sql.ConnectionPool(configWithSever3).connect().then((pool) => {
+    return pool;
+  });
+} else if (roleData.role == 2 && roleData.uniID == 4) {
+  conn = new sql.ConnectionPool(configWithSever4).connect().then((pool) => {
+    return pool;
+  });
+} else {
+  conn = new sql.ConnectionPool(config).connect().then((pool) => {
+    return pool;
+  });
+}
 
 module.exports = {
-    conn : conn,
-    sql: sql,
-}
+  conn: conn,
+  sql: sql,
+};
